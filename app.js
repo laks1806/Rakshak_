35b5d00e2e5d047eb8a478d7af5555a3ea93633f
Lakshita Singh <67590424+laks1806@users.noreply.github.com>
2021-04-26 01:43:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const passport = require('passport');
+const cookieSession = require('cookie-session')
+require('./passport-setup');
+
+
+// For an actual app you should configure this with an experation time, better keys, proxy and secure
+app.use(cookieSession({
+    name: 'tuto-session',
+    keys: ['key1', 'key2']
+  }))
+
+app.set('view engine','ejs')
+app.use(express.static(__dirname+'/Rakshak-rakshak'))
+
+// Auth middleware that checks if the user is logged in
+const isLoggedIn = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+// Initializes passport and passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Example protected and unprotected routes
+app.get('/index', (req, res) => res.render('index'))
+app.get('/login', (req, res) => res.render('login'))
+app.get('/chat', (req, res) => res.render('chat'))
+app.get('/audio', (req, res) => res.render('audio'))
+app.get('/register', (req, res) => res.render('register'))
+app.get('/selfdefence', (req, res) => res.render('selfdefence'))
+app.get('/table', (req, res) => res.render('table'))
+app.get('/video', (req, res) => res.render('video'))
+app.get('/untitled-1', (req, res) => res.render('untitled-1'))
+app.get('/profile', (req, res) => res.render('profile'))
+
+app.get('/failed', (req, res) => res.send('You Failed to log in!'))
+
+//In this route you can see that if the user is logged in u can acess his info in: req.user
+app.get('/profile', isLoggedIn, (req, res) =>{
+    res.render('/profile',{name:req.user.displayName,pic:req.user.photos[0].value,email:req.user.emails[0].value})
+})
+
+// Auth Routes
+app.get('/google', passport.authenticate('google', { scope: ['profile', 'email']}));
+
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/profile');
+   
+  }
+);
+
+app.get('/logout', (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
+})
+
+app.listen(5000, () => console.log(`Example app listening on port ${5000}!`))
